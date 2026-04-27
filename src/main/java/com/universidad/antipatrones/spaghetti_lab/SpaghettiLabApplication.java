f3bc322646b9688753c16566cc347ea363ff7691
Linda Bohorquez <96446503+LindaBohorquez@users.noreply.github.com>
2026-04-27 04:31:19 -0500

    primer commit

---
+++ src/main/java/com/universidad/antipatrones/spaghetti_lab/SpaghettiLabApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.antipatrones.spaghetti_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaghettiLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaghettiLabApplication.class, args);
+	}
+
+}
